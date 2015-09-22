34a81e242a3946f39ba9ec5791992def7e781699
gonzalo-rivas <gonzalo.rivas@anfix.com>
2015-09-22 12:31:25 +0200

    [ADD] Material

--- src/Select.js
+++ src/Select.js
@@ -861,10 +861,11 @@ var Select = React.createClass({
 		var focusedLabel = classes({
 			'Select-label' : true,
 			'focus' : this.state.isFocused,
-			'keep': !this.state.isFocused && val
+			'keep': !this.state.isFocused && val,
+			"aui-core-display-off": !this.props.material
 		});
 		
-		value = val && !this.props.material ? value : null;
+		value = val ? value : null;
 		
 		//classes
 		return (
